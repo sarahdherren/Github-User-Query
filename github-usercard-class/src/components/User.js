093fbde2fd42a46aa21105110b1a3b9d89a293c6
Sarah D Herren <herren.sarah@icloud.com>
2021-07-09 22:09:45 -0500

    mvp and stretch goals met ability to search github user

---
+++ github-usercard-class/src/components/User.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+export default function User(props) {
+
+
+    return (
+        <div>
+            <button className='reset' onClick={props.resetHandler}>
+                <GitHubIcon />
+                <span>find new user</span>
+            </button>
+            <div className='cardHeader'>
+                <div className='cardAvatar'>
+                    <img src={props.userData.avatar_url} alt='user avatar' />
+                </div>
+                <div className='cardTitle'>
+                    <h2>{props.userData.name}</h2>
+                    <p>{props.userData.login}</p>
+                    <h3>Followers:</h3>
+                    <div className='followers'>
+                            {props.followers.map(follower => (
+                                <div className='follower'>
+                                <div className='imgWrapper'>
+                                    <img src={follower.avatar_url} alt='follower avatar' />
+                                </div>
+                                <a href={`http://github.com/${follower.login}`}>{follower.login} </a>
+                                </div>
+                            ))}
+                    </div>
+                </div>
+            </div>
+            
+            <div className='repositories'>
+                <h3>Repositories:</h3>
+                    {props.repos.map(repo => (
+                            <ul>
+                                <li>
+                                    <a href={repo.html_url}>{repo.name} </a>
+                                </li>
+                            </ul>
+                        ))}
+            </div>
+        </div>
+    )
+}
