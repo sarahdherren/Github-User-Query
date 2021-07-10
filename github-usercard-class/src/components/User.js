35bc30082d449aafbb29ed20c6438050d8c82f8d
Sarah D Herren <herren.sarah@icloud.com>
2021-07-10 12:37:20 -0500

    added media queries

--- github-usercard-class/src/components/User.js
+++ github-usercard-class/src/components/User.js
@@ -19,8 +19,8 @@ export default function User(props) {
                     <p>{props.userData.login}</p>
                     <h3>Followers:</h3>
                     <div className='followers'>
-                        {props.followers.map(follower => (
-                            <div className='follower'>
+                        {props.followers.slice(0, 6).map((follower, index) => (
+                            <div className='follower' key={index}>
                             <div className='imgWrapper'>
                                 <img src={follower.avatar_url} alt='follower avatar' />
                             </div>
@@ -33,13 +33,13 @@ export default function User(props) {
             <div className='repoWrapper'>
             <h3>Repositories:</h3>
                 <div className='repositories'>
-                        {props.repos.map(repo => (
-                                <ul>
-                                    <li>
-                                        <a href={repo.html_url}>{repo.name} </a>
-                                    </li>
-                                </ul>
-                            ))}
+                    {props.repos.slice(0, 30).map((repo, index) => (
+                            <ul >
+                                <li>
+                                    <a href={repo.html_url} key={index}>{repo.name} </a>
+                                </li>
+                            </ul>
+                        ))}
                 </div>
             </div>
         </div>
