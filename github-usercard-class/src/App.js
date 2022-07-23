e796b3484050a45738a9ccfec7c518cf9fef2047
Sarah D Herren <67919714+sarahdherren@users.noreply.github.com>
2022-07-22 21:01:59 -0500

    corrects casing

--- github-usercard-class/src/App.js
+++ github-usercard-class/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './app.css';
 import axios from 'axios';
 import Header from './components/Header';
 import Form from './components/Form';
